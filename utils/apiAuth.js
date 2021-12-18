d505beaf1ce6adc3f6b031bf6b480efecdda1aee
Sebastian Chaca <69464726+SebastianChaca@users.noreply.github.com>
2021-12-18 02:11:38 -0300

    jwt expires

--- utils/apiAuth.js
+++ utils/apiAuth.js
@@ -5,7 +5,7 @@ exports.generateToken = (user) => {
     { id: user._id },
     process.env.SECRET_KEY || 'somethingsecret',
     {
-      expiresIn: process.env.JWT_EXPIRES,
+      expiresIn: '3d',
     }
   );
 };
